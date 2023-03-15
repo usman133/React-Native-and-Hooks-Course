076bd92b10104fe1484074360de46642d05d6adf
usman133 <usmankiani26@gmail.com>
2023-03-15 21:05:19 +1300

    FLex CSS

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,12 +2,28 @@ import Reat from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
-  return <View>
-  <Text>Box Screen</Text>
+  return (
+  <View styles={styles.viewStyle}>
+    <Text styles={styles.textStyle}>Child #1</Text>
+    <Text styles={styles.textStyle}>Child #2</Text>
+    <Text styles={styles.textStyle}>Child #3 - working</Text>
   </View>
 
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+        borderWidth: 10,
+        borderColor: 'black',
+        alignItems: 'center'
+  },
+
+  textStyle: {
+        borderWidth: 10,
+        borderColor: 'red'
+  }
+
+});
 
 export default BoxScreen;
